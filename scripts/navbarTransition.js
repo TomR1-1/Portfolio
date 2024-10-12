5a65235b8eca4be0aa137eae4ed12add4227015d
TomR1-1 <TomRotbart@gmail.com>
2024-10-12 05:47:33 -0700

    Updated website with new resume and some new pictures, need to add Ube as a project

--- scripts/navbarTransition.js
+++ scripts/navbarTransition.js
@@ -5,12 +5,13 @@ var nav = document.getElementById("navbar");
 window.onscroll = function(){
 
     if (window.pageYOffset > 100) {
-
+        console.log("Not at top of page")
         nav.style.background = "#333";
         nav.style.boxShadow = "0px 4px 2px #1c1c1c";
     }
     else{
-        nav.style.background = "transparent";
+        console.log("Top of page")
+        nav.style.background = "linear-gradient(to bottom, #333333CC 25%, #33333300)";
         nav.style.boxShadow = "none";
     }
 }
